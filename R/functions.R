049de72e498fcdb66d478f34d30a9108979aa14a
Maëlick Claes <himself@maelick.net>
2014-05-08 14:12:37 +0200

    FindFunctionsHash hash computation changed: do not compute hash recursively but rather compute it on body

--- R/functions.R
+++ R/functions.R
@@ -25,16 +25,16 @@ FindFunctionsList <- function(expr, threshold=0) {
 FindFunctionsHash <- function(expr, threshold=0, algo="sha1") {
   h <- hash()
   Ffunc <- function(args, body, ref, res, global, assign.name) {
+    force(res)
     func <- FuncDesc(ref, assign.name, args, body, global)
-    res <- digest(res, algo)
     if (MatchThreshold(func$body, threshold)) {
-      h[[res]] <- c(h[[res]], list(func))
+      key <- digest(body)
+      h[[key]] <- c(h[[key]], list(func))
     }
-    digest(list(args, res), algo)
   }
-  Fassign <- function(name, value, res, ...) digest(list(name, res), algo)
-  Fcall <- function(name, args, res, ...) digest(list(name, res), algo)
-  Fleaf <- function(value, ...) digest(value, algo)
+  Fassign <- function(name, res, ...) force(res)
+  Fcall <- function(name, args, res, ...) force(res)
+  Fleaf <- function(value, ...) NULL
   VisitExpressions(expr, list(Ffunc=Ffunc, Fassign=Fassign,
                               Fcall=Fcall, Fleaf=Fleaf),
                    global=TRUE)
