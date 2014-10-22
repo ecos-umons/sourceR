525f58563f5acfee801d06af21fcb01477bdfaad
Maëlick Claes <himself@maelick.net>
2014-10-22 15:29:51 +0200

    FunctionsHashToDataTable

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,16 @@ GuessEncoding <- function(filename) {
   cmd <- sprintf("file --mime-encoding %s", filename)
   strsplit(system(cmd, intern=TRUE), " ")[[1]][2]
 }
+
+FunctionsHashToDataTable <- function(hash) {
+  rbindlist(unlist(lapply(names(hash), function(h) {
+    lapply(hash[[h]], function(f) {
+      data.table(hash=h, file=f$file,
+                 begin.line=f$begin[1], begin.col=f$begin[2],
+                 end.line=f$end[1], end.col=f$end[2],
+                 name=as.character(f$name), args=list(f$args), body=f$body,
+                 is.global=f$is.global, size=f$size,
+                 loc=length(strsplit(f$body, "\\n")[[1]]))
+    })
+  }), recursive=FALSE))
+}
