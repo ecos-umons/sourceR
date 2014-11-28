f4f8ef64131facb3c80bb635ef18c2747f1cd929
Maëlick Claes <himself@maelick.net>
2014-11-28 12:04:31 +0100

    FuncDesc replaced by a data.table

--- R/functions.R
+++ R/functions.R
@@ -1,55 +1,27 @@
-FuncDesc <- function(ref, name, args, body, is.global, size=NA) {
+FunctionDescription <- function(hash, ref, name, args, body, is.global, size=NA) {
   file <- get("filename", attr(ref, "srcfile"))
-  begin <- ref[1:2]
-  end <- ref[3:4]
-  res <- list(file=file, begin=begin, end=end, name=name, args=args,
-              body=body, is.global=is.global, size=size)
-  class(res) <- "FuncDesc"
-  res
+  data.table(hash=hash, file=file,
+             begin.line=ref[1], begin.col=ref[2],
+             end.line=ref[3], end.col=ref[4],
+             name=as.character(name), args=list(args), body=body,
+             is.global=is.global, size=size,
+             loc=length(strsplit(body, "\\n")[[1]]))
 }
 
-FindFunctionsList <- function(expr, threshold=0) {
-  Ffunc <- function(args, body, ref, res, global, assign.name) {
-    func <- FuncDesc(ref, assign.name, args, body, global)
-    if (MatchThreshold(func$body, threshold)) {
-      c(list(func), res)
-    } else {
-      res
-    }
-  }
-  Fassign <- function(name, value, res, ...) res
-  Fcall <- function(name, args, res, ...) do.call(c, res)
-  Fleaf <- function(...) list()
-  unlist(VisitExpressions(expr, list(Ffunc=Ffunc, Fassign=Fassign,
-                                     Fcall=Fcall, Fleaf=Fleaf),
-                          global=TRUE),
-         recursive=FALSE)
-}
-
-FindFunctionsHash <- function(expr, threshold=0, algo="sha1") {
+FindFunctions <- function(expr, threshold=0, algo="sha1") {
   h <- hash()
-  Ffunc <- function(args, body, ref, res, global, assign.name) {
+  Ffunct <- function(args, body, ref, res, global, assign.name) {
     res <- sum(unlist(res)) + 1
-    func <- FuncDesc(ref, assign.name, args, body, global, res)
-    if (MatchThreshold(func$body, threshold)) {
-      key <- digest(body)
-      h[[key]] <- c(h[[key]], list(func))
-    }
+    key <- digest(body)
+    func <- FunctionDescription(key, ref, assign.name, args, body, global, res)
+    h[[key]] <- c(h[[key]], list(func))
     res
   }
   Fassign <- function(name, res, ...) res + 1
   Fcall <- function(name, args, res, ...) sum(unlist(res)) + 1
   Fleaf <- function(value, ...) 1
-  VisitExpressions(expr, list(Ffunc=Ffunc, Fassign=Fassign,
-                              Fcall=Fcall, Fleaf=Fleaf),
+  VisitExpressions(expr, list(funct=Ffunct, assign=Fassign,
+                              call=Fcall, leaf=Fleaf),
                    global=TRUE)
-  h
-}
-
-FindFunctions <- function(expr, threshold=0, algo="sha1", use.hash=FALSE) {
-  if (use.hash) {
-    FindFunctionsHash(expr, threshold, algo)
-  } else {
-    FindFunctionsList(expr, threshold)
-  }
+  rbindlist(unlist(as.list(h), recursive=FALSE))
 }
--- R/visit.R
+++ R/visit.R
@@ -3,25 +3,25 @@ VisitExpression <- function(e, FUNC, global=FALSE, assign.name=NA) {
     name <- e[[1]]
     if (name == "function") {
       body <- as.character(as.expression(e[[3]]))
-      FUNC$Ffunc(args=e[[2]], body=body, ref=getSrcref(e[[4]]),
-                 res=VisitExpression(e[[3]], FUNC),
-                 global=global, assign.name=assign.name)
+      FUNC$funct(args=e[[2]], body=body, ref=getSrcref(e[[4]]),
+                    res=VisitExpression(e[[3]], FUNC),
+                    global=global, assign.name=assign.name)
     } else if (name == "<-" | name == "=") {
-      FUNC$Fassign(name=e[[2]], value=e[[3]],
-                   res=VisitExpression(e[[3]], FUNC, global, e[[2]]),
-                   global=global, assign.name=assign.name)
+      FUNC$assign(name=e[[2]], value=e[[3]],
+                  res=VisitExpression(e[[3]], FUNC, global, e[[2]]),
+                  global=global, assign.name=assign.name)
     } else {
       args <- as.list(e)[2:length(e)]
       if (name == "{" & length(args) == 1) {
         do.call(c, VisitExpressions(args, FUNC))
       } else {
-        FUNC$Fcall(name=name, args=args,
-                   res=lapply(args, VisitExpression, FUNC),
-                   global=global, assign.name=assign.name)
+        FUNC$call(name=name, args=args,
+                  res=lapply(args, VisitExpression, FUNC),
+                  global=global, assign.name=assign.name)
       }
     }
   } else {
-    FUNC$Fleaf(value=e, global=global, assign.name=assign.name)
+    FUNC$leaf(value=e, global=global, assign.name=assign.name)
   }
 }
 
