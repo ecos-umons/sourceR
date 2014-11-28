8a6d4650c432a0c0ea589d659ee8ca3bf5d1fa17
Maëlick Claes <himself@maelick.net>
2014-11-28 12:04:55 +0100

    ParseFile and ParseText functions added

--- R/packages.R
+++ R/packages.R
@@ -3,10 +3,15 @@ ParsePackage <- function(package, guess.encoding=FALSE) {
   files <- dir(path, full.names=TRUE, recursive=TRUE)
   src <- grep("\\.R$", files, ignore.case=TRUE, value=TRUE)
   if (length(src)) {
-    do.call(c, lapply(src, function(filename) {
-      encoding <- "unknown"
-      if (guess.encoding) encoding <- GuessEncoding(filename)
-      parse(filename, keep.source=TRUE, encoding=encoding)
-    }))
+    do.call(c, lapply(src, ParseFile))
   }
+}
+
+ParseFile <- function(filename, guess.encoding=FALSE) {
+  encoding <- if (guess.encoding) GuessEncoding(filename) else "unknown"
+  parse(filename, keep.source=TRUE, encoding=encoding)
+}
+
+ParseText <- function(text) {
+  parse(text=text, keep.source=FALSE)
 }
