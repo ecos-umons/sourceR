6066a32520b1c928442baff30735cdf502f8f293
Maëlick Claes <himself@maelick.net>
2015-02-23 13:53:42 +0100

    Removed unused util functions

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,5 @@
-MatchThreshold <- function(expr, threshold) {
-  length(strsplit(as.character(as.expression(expr)),
-                  "\n")[[1]]) >= threshold
-}
-
 GuessEncoding <- function(filename) {
   # Guesses the encoding of a file.
   cmd <- sprintf("file --mime-encoding %s", filename)
   strsplit(system(cmd, intern=TRUE), " ")[[1]][2]
-}
-
-FunctionsHashToDataTable <- function(hash) {
-  rbindlist(unlist(lapply(names(hash), function(h) {
-    lapply(hash[[h]], function(f) {
-      data.table(hash=h, file=f$file,
-                 begin.line=f$begin[1], begin.col=f$begin[2],
-                 end.line=f$end[1], end.col=f$end[2],
-                 name=as.character(f$name), args=list(f$args), body=f$body,
-                 is.global=f$is.global, size=f$size,
-                 loc=length(strsplit(f$body, "\\n")[[1]]))
-    })
-  }), recursive=FALSE))
 }
