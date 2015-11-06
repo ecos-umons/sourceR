289486c90f315f72f19919d6b9b4d5f726c78bb0
Maëlick Claes <himself@maelick.net>
2015-11-06 14:10:49 +0100

    Calls test added

--- tests/testthat/test_calls.R
+++ tests/testthat/test_calls.R
@@ -17,6 +17,22 @@ package:::f2()
   expect_equal(calls$package, c(NA, NA, "package", "package"))
 })
 
+test_that("Calls with common", {
+  code <- ParseText("
+f1(y, x=1)
+f1 <- 2
+f1()
+f2()
+f2 <- function() f1
+f2()
+package::f2()
+package:::f2()
+1 + 1")
+  calls <- FunctionCalls(code)
+  expect_equal(calls$name, c("f1", "f2", "f2", "f2"))
+  expect_equal(calls$package, c(NA, NA, "package", "package"))
+})
+
 test_that("Calls without common", {
   code <- ParseText("
 f1(y, x=1)
